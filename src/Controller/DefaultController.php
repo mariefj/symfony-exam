40f24afa1e6764a2828b74d6d5f1110e20d3ec8d
MarieF118 <72809324+MarieF118@users.noreply.github.com>
2021-03-09 15:36:50 +0100

    Doctrine (entités, enregistrement des données, récup) - erreur pour recup la date

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,22 +5,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ContactRepository;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(ContactRepository $repo): Response
     {
-        return $this->render('default/index.html.twig');
+        $contacts = $repo->findAll();
+        
+        /* Si on veut rechercher par format d'email :
+        $contacts = $repo->findByEmail("test@test.com"); */
+
+        return $this->render('default/index.html.twig', [
+            'contacts' => $contacts
+        ]);
     }
 
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(): Response
+    public function contact(EntityManagerInterface $entityManager): Response
     {
+        $contact = new Contact();
+    
+        $contact->setEmail("test@test.com")
+                ->setSubject("Ceci est un test")
+                ->setMessage("Un message de test, pouvant être long, ou non. Celui-ci ne l'est pas :)");
+
+        $entityManager->persist($contact);
+        $entityManager->flush();
+
         return $this->render('default/contact.html.twig');
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+use DateTime;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    public function __construct()
+    {
+        $this->created_at = new DateTime();
+    }
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
