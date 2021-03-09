976f4480755e9fceb6cade5b69260007e1904832
MarieF118 <72809324+MarieF118@users.noreply.github.com>
2021-03-09 14:19:50 +0100

    controller et vues avec twig

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('default/contact.html.twig');
+    }
+}
